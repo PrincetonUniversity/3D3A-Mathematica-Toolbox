b79cd35012e9391d4c373f334a5fd1bc1e6613ac
Joe Tylka <josephgt@princeton.edu>
2018-11-09 14:51:21 -0500

    Add MIT license boilerplate

--- mathlink/makeMinimumPhase/makeMinimumPhase.c
+++ mathlink/makeMinimumPhase/makeMinimumPhase.c
@@ -1,12 +1,42 @@
 //
 //  makeMinimumPhase.c
 //  Minimum-phase conversion routine for real valued, 1-D lists in Mathematica.
+
+// ==============================================================================
+// This file is part of the 3D3A Mathematica Toolbox.
+//
+// Joseph G. Tylka <josephgt@princeton.edu>
+// 3D Audio and Applied Acoustics (3D3A) Laboratory
+// Princeton University, Princeton, New Jersey 08544, USA
+//
+// MIT License
+//
+// Copyright (c) 2018 Princeton University
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// ==============================================================================
+
+//  Version History:
 //
 //  Version 1.0: adapted from BacchDSP.c in LibBacchAD_v1.65.
 //
 //  Created by Joe Tylka on 19 March 2016.
-//  Copyright (c) 2016 Joe Tylka. All rights reserved.
-//
 
 #define EPS 0.000000000000000152655665885959
 #include <stdio.h>
