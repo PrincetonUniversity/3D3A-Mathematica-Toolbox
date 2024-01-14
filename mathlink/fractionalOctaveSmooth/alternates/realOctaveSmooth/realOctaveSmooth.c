e41ec7943a89d4a5f7d4c392527acf86b46fbd14
J. Tylka <joe.tylka@siemens.com>
2024-01-14 17:45:45 -0500

    Update to v2.2

--- mathlink/fractionalOctaveSmooth/alternates/realOctaveSmooth/realOctaveSmooth.c
+++ mathlink/fractionalOctaveSmooth/alternates/realOctaveSmooth/realOctaveSmooth.c
@@ -2,6 +2,8 @@
 //  fractionalOctaveSmooth.c
 //  Fractional Octave Smoothing routine for real valued, 1-D lists in Mathematica.
 //
+//  Version 2.2: Removed window creating functions, instead compute the value of the point in the window before each computation. Change uplpos computation to only calculate those values which are less than Len.
+//
 //  Version 2.1: Changed calloc to malloc. Removed "out" pointer. Removed n = 0.0 in createHanning(). Added exact window normalization instead of computing it in createHanning().
 //
 //  Version 2.0: Changed lowlposv and uplposv to lowlpos and uplpos - no longer length Len vectors, but single integers. Freed allocated memory for outvec and win.
@@ -17,10 +19,6 @@
 #include <stdlib.h>
 #include <complex.h>
 #include <math.h>
-
-void realOctaveSmooth(double fractionDenominator, int windowType);
-void createHanning(int N, double* win);
-void createRectangular(int N, double* win);
 
 int main(int argc, char *argv[])
 {
@@ -39,8 +37,14 @@ void realOctaveSmooth(double fractionDenominator, int windowType)
     int lowlpos = 0;
     int uplpos = 0;
     int winLen = 0;
-    double temp = 0.0;
+    int firstCap = 0;
+    double n = 0.5;
+    double iiDouble = 0.0;
+    double LenDouble = 0.0;
     double partsum = 0.0;
+    double win = 0.0;
+    double winLenDouble = 0.0;
+    double twopi = 2.0 * M_PI;
     double indexRatio = exp2(1.0/fractionDenominator);
         
     /* Get Inputs from Mathematica */
@@ -54,76 +58,58 @@ void realOctaveSmooth(double fractionDenominator, int windowType)
         return;
     }
     
-    /* More Local Variables */
-    double *win = malloc(sizeof(double) * Len);
-    
     /* Outputs */
     double *outvec = malloc(sizeof(double) * Len);
     
     /* Begin Real Code */
+    LenDouble = (double) Len;
+    firstCap = floor(LenDouble / indexRatio);
     for(ii = 0; ii < Len; ii = ii + 1)
     {
         // Compute 1/N octave bounds
-        lowlpos = floor(ii / indexRatio);
-        uplpos = ceil(ii * indexRatio);
-        if(uplpos >= Len)
+        lowlpos = floor(iiDouble / indexRatio);
+        if(ii < firstCap)
+        {
+            uplpos = ceil(iiDouble * indexRatio);
+        }
+        else
         {
             uplpos = Len - 1;
         }
         
         winLen = uplpos - lowlpos + 1;
-        
-        // Create Window
-        switch(windowType)
-        {
-            case 0:
-                createRectangular(winLen,win);
-                break;
-            default:
-                createHanning(winLen,win);
-                break;
-        }
+        winLenDouble = (double) winLen;
         
         // Compute sum of window x data
-        for(jj = 0; jj < winLen; jj = jj + 1)
+        switch(windowType)
         {
-            partsum = partsum + data[lowlpos + jj] * win[jj];
+            case 0: // Rectangular Window
+                win = 1.0/winLenDouble;
+                for(jj = 0; jj < winLen; jj = jj + 1)
+                {
+                    partsum = partsum + data[lowlpos + jj] * win;
+                }
+                break;
+            default: // Hanning Window
+                for(jj = 0; jj < winLen; jj = jj + 1)
+                {
+                    win = (1 - cos(twopi * n / winLenDouble))/winLenDouble;
+                    partsum = partsum + data[lowlpos + jj] * win;
+                    n = n + 1.0;
+                }
+                n = 0.5;
+                break;
         }
         outvec[ii] = partsum;
         partsum = 0.0;
+        iiDouble = iiDouble + 1.0;
     }
     
     /* Send Output to Mathematica */
     MLPutReal64List(stdlink, outvec, Len);
     
     /* Free Memory */
-    free(win);
     free(outvec);
     MLReleaseReal64List(stdlink, data, Len);
     return;
-}
-
-void createHanning(int N, double* win)
-{
-    int ii = 0;
-    double n = 0.5;
-    double Ntemp = (double) N;
-    double twopi = 2.0 * M_PI;
-    
-    for(ii = 0; ii < N; ii = ii + 1)
-    {
-        win[ii] = (1 - cos(twopi * n / Ntemp))/Ntemp;
-        n = n + 1.0;
-    }
-}
-
-void createRectangular(int N, double* win)
-{
-    int ii = 0;
-    double Ntemp = (double) N;
-    
-    for(ii = 0; ii < N; ii = ii + 1)
-    {
-        win[ii] = 1.0/Ntemp;
-    }
 }
