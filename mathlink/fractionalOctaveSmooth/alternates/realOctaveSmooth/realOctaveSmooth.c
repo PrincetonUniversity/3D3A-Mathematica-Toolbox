4a5a9781cc8f18cf4c99d2bbecf3a31e708004e4
J. Tylka <joe.tylka@siemens.com>
2024-01-14 17:45:29 -0500

    Update to v2.1

--- mathlink/fractionalOctaveSmooth/alternates/realOctaveSmooth/realOctaveSmooth.c
+++ mathlink/fractionalOctaveSmooth/alternates/realOctaveSmooth/realOctaveSmooth.c
@@ -2,6 +2,8 @@
 //  fractionalOctaveSmooth.c
 //  Fractional Octave Smoothing routine for real valued, 1-D lists in Mathematica.
 //
+//  Version 2.1: Changed calloc to malloc. Removed "out" pointer. Removed n = 0.0 in createHanning(). Added exact window normalization instead of computing it in createHanning().
+//
 //  Version 2.0: Changed lowlposv and uplposv to lowlpos and uplpos - no longer length Len vectors, but single integers. Freed allocated memory for outvec and win.
 //
 //  Version 1.0: fractionDenominator must be of type double. windowType = 0 corresponds to a rectangular window, while any other value corresponds to a Hanning window. Other window types are easily added by adding a new function and defining a type number.
@@ -36,7 +38,7 @@ void realOctaveSmooth(double fractionDenominator, int windowType)
     int jj = 0;
     int lowlpos = 0;
     int uplpos = 0;
-    int tempLen = 0;
+    int winLen = 0;
     double temp = 0.0;
     double partsum = 0.0;
     double indexRatio = exp2(1.0/fractionDenominator);
@@ -44,6 +46,7 @@ void realOctaveSmooth(double fractionDenominator, int windowType)
     /* Get Inputs from Mathematica */
     MLGetReal64List(stdlink, &data, &Len);
     
+    // No Smoothing Case
     if(fractionDenominator == 0.0)
     {
         MLPutReal64List(stdlink, data, Len);
@@ -52,11 +55,10 @@ void realOctaveSmooth(double fractionDenominator, int windowType)
     }
     
     /* More Local Variables */
-    double *win = calloc(Len, sizeof(double));
+    double *win = malloc(sizeof(double) * Len);
     
     /* Outputs */
-    double *outvec = calloc(Len, sizeof(double));
-    double *out;
+    double *outvec = malloc(sizeof(double) * Len);
     
     /* Begin Real Code */
     for(ii = 0; ii < Len; ii = ii + 1)
@@ -69,30 +71,27 @@ void realOctaveSmooth(double fractionDenominator, int windowType)
             uplpos = Len - 1;
         }
         
-        tempLen = uplpos - lowlpos + 1;
+        winLen = uplpos - lowlpos + 1;
         
         // Create Window
         switch(windowType)
         {
             case 0:
-                createRectangular(tempLen,win);
+                createRectangular(winLen,win);
                 break;
             default:
-                createHanning(tempLen,win);
+                createHanning(winLen,win);
                 break;
         }
         
         // Compute sum of window x data
-        for(jj = 0; jj < tempLen; jj = jj + 1)
+        for(jj = 0; jj < winLen; jj = jj + 1)
         {
             partsum = partsum + data[lowlpos + jj] * win[jj];
         }
         outvec[ii] = partsum;
         partsum = 0.0;
     }
-    
-    /* Format Output */
-    out = outvec;
     
     /* Send Output to Mathematica */
     MLPutReal64List(stdlink, outvec, Len);
@@ -109,22 +108,13 @@ void createHanning(int N, double* win)
     int ii = 0;
     double n = 0.5;
     double Ntemp = (double) N;
-    double partsum = 0.0;
     double twopi = 2.0 * M_PI;
     
     for(ii = 0; ii < N; ii = ii + 1)
     {
-        win[ii] = 0.5*(1 - cos(twopi * n / Ntemp));
-        partsum = partsum + win[ii];
+        win[ii] = (1 - cos(twopi * n / Ntemp))/Ntemp;
         n = n + 1.0;
     }
-    n = 0.0;
-    
-    for(ii = 0; ii < N; ii = ii + 1)
-    {
-        win[ii] = win[ii]/partsum;
-    }
-    
 }
 
 void createRectangular(int N, double* win)
